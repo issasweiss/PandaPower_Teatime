0acdc9b923cb055480b733a9def27a9609b7318f
issasws <issasweiss01@gmail.com>
2023-09-25 19:55:30 +0300

    edit

--- Smart_Grids.py
+++ Smart_Grids.py
@@ -15,17 +15,19 @@ from pandapower.timeseries import DFData
 import os.path
 from openpyxl.workbook import Workbook
 
+
 def time_series(output_dir):
     profiles, ds = create_data_source()
-    time_steps = len(profiles.index)
+    time_steps = profiles.index
     area_x_buses, area_x_lines, net_area1 = get_network_area()
-    profiles, ds = create_data_source()
-    create_controllers(net_area1, ds)
+    net = load_network()
+    create_controllers(net, ds)
 
-    create_output_writer(net_area1, output_dir=output_dir, profiles = profiles)
-    run_timeseries(net_area1, time_steps)
+    create_output_writer(net, output_dir, profiles)
 
-    return print(profiles), print(net_area1)
+    return run_timeseries(net_area1, time_steps)
+
+
 
 def create_data_source():
     profiles = pd.read_csv("/Users/admin/Downloads/timeseries_exercise_5.csv", delimiter=";", index_col=0)
@@ -34,26 +36,31 @@ def create_data_source():
 
     return profiles, ds
 
+
 def create_controllers(net, ds):
-    area_x_buses, area_x_lines , net_area1 = get_network_area()
+    area_x_buses, area_x_lines, net_area1 = get_network_area()
 
     ConstControl(net, "load", variable='p_mw', element_index=net_area1.load.index,
                  data_source=ds, profile_name=["loads"])
-    ConstControl(net,  "sgen" ,variable='p_mw' ,element_index=net_area1.sgen.index,
+    ConstControl(net, "sgen", variable='p_mw', element_index=net_area1.sgen.index,
                  data_source=ds, profile_name=["sgens"])
 
-def create_output_writer(net_area1, output_dir, profiles):
 
-    time_steps = len(profiles.index)
-    area_x_buses, area_x_lines , net_area1 = get_network_area()
-    ow = OutputWriter(net_area1, time_steps = time_steps, output_path=output_dir, output_file_type=".xlsx", log_variables=list())
+def create_output_writer(net, output_dir, profiles):
+    time_steps = profiles.index
+    ow = OutputWriter(net, time_steps, output_path=output_dir, output_file_type=".xlsx",
+                      log_variables=list())
 
-    ow.log_variable('res_bus', 'vm_pu', index=area_x_buses, eval_function=np.max, eval_name="bus_max_vm_pu")
-    ow.log_variable('res_bus', 'vm_pu', index=area_x_buses, eval_function=np.min, eval_name="bus_min_vm_pu")
-    ow.log_variable('res_line', 'p_mw', index=area_x_lines, eval_function=np.max, eval_name="line_max_loading")
+    ow.log_variable('res_bus', 'vm_pu')# index=area_x_buses.index, eval_function=max, eval_name="bus_max_vm_pu")
+    ow.log_variable('res_bus', 'vm_pu') #index=area_x_buses.index, eval_function=min, eval_name="bus_min_vm_pu")
+    ow.log_variable('res_line', 'p_mw') #index=area_x_lines.index, eval_function=max, eval_name="line_max_loading")
+
     return ow
+
+
 def load_network():
     return pp.from_json("/Users/admin/Downloads/Excercises/net_exercise_5.json")
+
 
 def get_network_area():
     net = load_network()
@@ -68,6 +75,7 @@ def get_network_area():
 
     return area_x_buses, area_x_lines, net_area1
 
+
 def plotting_area():
     net = load_network()
     area_x_buses, area_x_lines, net_area1 = get_network_area()
@@ -75,7 +83,8 @@ def plotting_area():
     bc = plot.create_bus_collection(net, net.bus.index, size=80, color="grey", zorder=3)
     bch = plot.create_bus_collection(net, area_x_buses, size=80, color="green", zorder=3)
     lcl = plot.create_line_collection(net, net.line.index, zorder=1, color="grey")
-    sh = plot.create_bus_collection(net, [net.ext_grid.bus.loc[0]], patch_type="rect", size=200, color="green",zorder=11)
+    sh = plot.create_bus_collection(net, [net.ext_grid.bus.loc[0]], patch_type="rect", size=200, color="green",
+                                    zorder=11)
     lc = plot.create_line_collection(net, area_x_lines, zorder=2, color="green")
 
     plot.draw_collections([bc, bch, lc, lcl, sh], figsize=(8, 6))
@@ -97,9 +106,9 @@ def violations(net):
 
 def plot_timeseries(output_dir):
     # voltage results
-    area_x_buses, area_x_lines , net_area1 = get_network_area()
+    area_x_buses, area_x_lines, net_area1 = get_network_area()
     profiles = pd.read_csv("/Users/admin/Downloads/timeseries_exercise_5.csv", delimiter=";", index_col=0)
-    create_output_writer(net_area1,"/Users/admin/Downloads/Excercises", profiles )
+    create_output_writer(net_area1, "/Users/admin/Downloads/Excercises", profiles)
     vm_pu_file = os.path.join(output_dir, "res_bus", "vm_pu.xlsx")
     vm_pu = pd.read_excel(vm_pu_file, index_col=0)
     vm_pu.plot(label="vm_pu_max")
@@ -130,16 +139,16 @@ def plot_timeseries(output_dir):
 
     return
 
-output_dir = os.path.join(tempfile.gettempdir(), "/Users/admin/Downloads/Excercises")
+
+output_dir = "/Users/admin/Downloads/Results"
 print("Results can be found in your local temp folder: {}".format(output_dir))
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 #time_series(output_dir)
 #plot_timeseries("/Users/admin/Downloads/Excercises")
-#get_network_area()
-#plotting_area()
-
-#create_data_source()
-
-
-plot_timeseries("/Users/admin/Downloads/Excercises")
+# get_network_area()
+# plotting_area()
+# create_data_source()
+area_x_buses, area_x_lines, net_area1 = get_network_area()
+profiles = pd.read_csv("/Users/admin/Downloads/timeseries_exercise_5.csv", delimiter=";", index_col=0)
+create_output_writer(net_area1, output_dir, profiles)
